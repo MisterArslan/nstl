741f30b76834febeaa2f72b84cbb7383819756cc
MisterArslan <arsburs@gmail.com>
2019-04-02 13:27:42 +0300

    global refactoring

---
+++ tests/test.cpp
@@ -0,0 +1,12 @@
+#include "LinkedList.h"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestReverse)
+
+BOOST_AUTO_TEST_CASE(TestList) {
+	BOOST_REQUIRE_EQUAL(1, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
