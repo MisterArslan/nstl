1441d908f1dbb76c6edcc0db4d0688a195054037
MisterArslan <arsburs@gmail.com>
2019-04-10 18:24:55 +0300

    implemented list.h base

--- include/list.h
+++ include/list.h
@@ -4,39 +4,63 @@
 #include "allocator.h"
 
 namespace nstl{
-    template<class T, class A = nstl::allocator<T>>
-    class list{
-    public:
-        list() = default;
-        list(const list&) noexcept {
-
-        }
-        list(list&& list) noexcept {
-
-        }
-        list& operator=(const list& other) noexcept {
-
-        }
-        ~list(){
-
-        }
+    template<class T, class Allocator = nstl::allocator<T>>
+    class list {
     private:
         struct node {
             T value;
             node *next;
             node *prev;
         };
+    public:
+        list() = default;
+        list(const list&) noexcept {
+            auto temp = list::front();
+            while(temp) {
+                push_back(temp->value);
+                temp = temp->next;
+            }
+        }
+        list(list&& list) noexcept {
+            this = list;
+        }
 
+        node* front() {return head;}
+        node* back() {return tail;}
+
+        void push_front(T value) {
+            if (head) {
+                auto temp = createNode(value);
+                temp->next = head;
+                head = temp;
+            } else {
+                head = createNode(value);
+                tail = head;
+            }
+        }
+
+        void push_back(T value) {
+            if (tail) {
+                tail->next = createNode(value);
+            } else {
+                head = createNode(value);
+                tail = head;
+            }
+        }
+
+        ~list(){
+
+        }
+    private:
         node* head;
         node* tail;
 
-        node* newNode(T value){
+        node* createNode(T value){
             node* newNode = new node;
             newNode->value = value;
             newNode->next = nullptr;
             newNode->prev = nullptr;
         }
-
     };
 }
 
